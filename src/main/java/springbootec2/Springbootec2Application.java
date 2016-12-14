03b07ff5b8d8ce93ca74ee800589e98b5f010178
Scott Braaten <sbraaten95@gmail.com>
2016-12-14 16:43:03 -0500

    first commit

---
+++ src/main/java/springbootec2/Springbootec2Application.java
@@ -0,0 +1,12 @@
+package springbootec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootec2Application.class, args);
+	}
+}
---
+++ src/test/java/springbootec2/Springbootec2ApplicationTests.java
@@ -0,0 +1,18 @@
+package springbootec2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Springbootec2Application.class)
+@WebAppConfiguration
+public class Springbootec2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
